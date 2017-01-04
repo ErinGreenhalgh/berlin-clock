827f910e0caf891767c538d8fe32342860989634
Erin Greenhalgh <erin.greenhalgh0113@gmail.com>
2017-01-04 15:13:40 -0700

    ensure project compiles with a main method and a first, trivial test

---
+++ src/test/java/ClockTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ClockTest {
+    @Test
+    public void firstTest(){
+        boolean truth = true;
+        assertEquals(truth, true);
+    }
+
+
+
+}
