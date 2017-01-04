277d5ed934a3d4b3bfd32c241f3ae63ed96ed037
Erin Greenhalgh <erin.greenhalgh0113@gmail.com>
2017-01-04 16:00:33 -0700

    create first few tests for Hours class: * Hours takes in an number of hours as an int * also contains top and bottom row instance vars that are arrays of a length of 4 * these top and bottom rows are all off by default

---
+++ src/test/java/HoursTest.java
@@ -0,0 +1,50 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
+public class HoursTest {
+    @Test
+    public void takesInANumberOfHours() {
+        Hours  h = new Hours(15);
+        assertEquals(h.getNumber(), 15);
+    }
+
+    @Test
+    public void hasATopAndBottomRow(){
+        Hours h = new Hours (1);
+        String [] expectedTop = new String[4];
+        String [] expectedBottom = new String[4];
+
+        assertArrayEquals(h.getTopRow(), expectedTop);
+        assertArrayEquals(h.getBottomRow(), expectedBottom);
+    }
+
+    @Test
+    public void rowsDefaultToAllOff(){
+        Hours h = new Hours(1);
+        h.setTopRow();
+
+        String[] expectedTop = new String[4];
+        expectedTop[0] = "O";
+        expectedTop[1] = "O";
+        expectedTop[2] = "O";
+        expectedTop[3] = "O";
+
+        assertArrayEquals(h.getTopRow(), expectedTop);
+    }
+
+    @Test
+    public void canTurnOnTopRow(){
+        Hours h = new Hours(10);
+        h.setTopRow();
+
+        String[] expected = new String[4];
+        expected[0] = "R";
+        expected[1] = "R";
+        expected[2] = "O";
+        expected[3] = "O";
+
+        assertArrayEquals(h.getTopRow(), expected);
+     }
+}
