6c90c8416bcf6187e6792641f7b09ce95f65f088
Erin Greenhalgh <erin.greenhalgh0113@gmail.com>
2017-05-12 10:33:22 -0600

    display hours representation from Clock class
    * right now, the clock takes in a single number of hours from the command line, passes that to the Hours class to get a representation, and prints out that representation, using the RowConverter class to make it easier to print the representation as a string
    * obviously no error handling right now, just getting something to display

--- src/main/java/Clock.java
+++ src/main/java/Clock.java
@@ -1,5 +1,17 @@
 public class Clock {
     public static void main(String[] args){
-        System.out.println("Hello Clock!");
+        //read in a number of hours from the command line
+        String numberOfHours = args[0];
+        Hours hoursCalculator = new Hours();
+
+        //pass that number to Hours class to get a representation
+        String[][] hours = hoursCalculator.display(Integer.parseInt(numberOfHours));
+
+        //print out the representation
+        RowConverter converter = new RowConverter();
+        for (String[] row: hours) {
+            String displayData = converter.convert(row);
+            System.out.println(displayData);
+        }
     }
 }
